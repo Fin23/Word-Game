cd18d59ec91ce00690432c109943d982ece78f1b
Ross <rossjameson1623@gmail.com>
2019-06-17 15:25:03 -0500

    need some extra java script at the end

--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,5 @@
+//Global Variables
+//=============================================================
 //create an array of words
 const words = ['Rockos Modern Life', 'Ren and Stimpy', 'Aaahh! Real Monsters', 'Doug', 'Gargoyles', 'Hey Arnold', 'Doug', 'Darkwing Duck', 'daria', 'Pinky and The Brain']
 //choose word randomly
@@ -9,6 +11,11 @@ let underScore = [];
 
 //testing
 console.log(choosenWord)
+
+//main
+//===========================================================
+
+
 //create underscores based on length of word
 let generateUnderscore = ( ) => {
     for ( let i = 0; i< choosenWord.length; i++){
@@ -18,28 +25,22 @@ let generateUnderscore = ( ) => {
 }
 
 
-//get users guess using key code/ index of method
-
+//get users guess
+// using key code/ index of method
 document.addEventListener('keypress', (event) => {
     let keyword = String.fromCharCode(event.keyCode);
 
     //if users guess is right 
-
     if(choosenWord.indexOf(keyword) > -1){
-
         //add to right words array
-
         rightWord.push(keyword);
-
         //replace underscore with right letter
 underScore[choosenWord.indexOf(keyword)] = keyword;
 //checks to see if user word matches guesses
 if(underScore.join('') == choosenWord){
     alert('Winner Winner Chicken Dinner');
-}
-
-
     }
+}
     else wrongWord.push(keyword);
     console.log(wrongWord);
 })
