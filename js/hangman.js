bb6e73a106a961d47411385163c7ddebfc55ae2c
Ross <rossjameson1623@gmail.com>
2019-06-17 12:06:57 -0500

    added more javascript

--- js/hangman.js
+++ js/hangman.js
@@ -3,7 +3,12 @@ const words = ['Rockos Modern Life', 'Ren and Stimpy', 'Aaahh! Real Monsters', '
 //choose word randomly
 let randNum = Math.floor(Math.random() * words.length);
 let choosenWord = words[randNum];
+let rightWord = [];
+let wrongWord = [];
 let underScore = [];
+
+//testing
+console.log(choosenWord)
 //create underscores based on length of word
 let generateUnderscore = ( ) => {
     for ( let i = 0; i< choosenWord.length; i++){
@@ -13,7 +18,31 @@ let generateUnderscore = ( ) => {
 }
 
 
-//get users guess
+//get users guess using key code/ index of method
+
+document.addEventListener('keypress', (event) => {
+    let keyword = String.fromCharCode(event.keyCode);
+
+    //if users guess is right 
+
+    if(choosenWord.indexOf(keyword) > -1){
+
+        //add to right words array
+
+        rightWord.push(keyword);
+
+        //replace underscore with right letter
+underScore[choosenWord.indexOf(keyword)] = keyword;
+//checks to see if user word matches guesses
+if(underScore.join('') == choosenWord){
+    alert('Winner Winner Chicken Dinner');
+}
+
+
+    }
+    else wrongWord.push(keyword);
+    console.log(wrongWord);
+})
 //check if guess is right 
 // if right push to right array
 // if wrong push to wrong array
